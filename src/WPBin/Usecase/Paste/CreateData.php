a921458360b851824c3af8875b134d96ad4d12bf
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-06 15:45:08 -0400

    add paste createdata spec & impl

---
+++ src/WPBin/Usecase/Paste/CreateData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WPBin\Usecase\Paste;
+
+use WPBin\Data;
+
+class CreateData extends Data
+{
+    public $title;
+    public $content;
+    public $parent_id;
+    public $user_id;
+}
