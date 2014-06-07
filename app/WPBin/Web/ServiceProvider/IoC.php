01157c23dac8a18e2dc4b837639502a4f0e98fa9
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-07 00:38:21 -0400

    add IoC service provider

---
+++ app/WPBin/Web/ServiceProvider/IoC.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WPBin\Web\ServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class IoC extends ServiceProvider {
+    public function register()
+    {
+        // $this->app->bind(
+        //     'WPBin\Web\Repository\TagInterface',
+        //     'WPBin\Web\Repository\Tag'
+        // );
+    }
+}
--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+		'WPBin\Web\ServiceProvider\IoC',
+
 	),
 
 	/*
