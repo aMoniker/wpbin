e89f0b1a25c031794016197097307fbedf7e17e2
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-08 18:22:27 -0400

    usecases shouldnt depend on the framework

--- app/WPBin/Web/Repository/Tag/Create.php
+++ app/WPBin/Web/Repository/Tag/Create.php
@@ -3,21 +3,22 @@
 namespace WPBin\Web\Repository\Tag;
 
 use WPBin\Core\Entity\Tag;
+use WPBin\Core\Usecase\Tag\CreateRepository;
+
 use WPBin\Web\Repository;
 use WPBin\Web\Models\Tag as TagModel;
-
-use WPBin\Core\Usecase\Tag\CreateRepository;
 
 class Create extends Repository implements CreateRepository
 {
     public function create(Tag $entity)
     {
-        $tag = new TagModel(array(
+        $model = new TagModel(array(
             'name' => $entity->get('name'),
             'url'  => $entity->get('url')
         ));
-        $tag->save();
+        $model->save();
 
-        return \App::make('WPBin\Core\Entity\Tag', [$tag->toEntityArray()]);
+        $entity->setByArray($model->toEntityArray());
+        return $entity;
     }
 }
