e9431a5361eedf1a62c799c13d537c4963f1c057
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-08 00:05:40 -0400

    IoC bindings

--- app/WPBin/Web/ServiceProvider/IoC.php
+++ app/WPBin/Web/ServiceProvider/IoC.php
@@ -5,11 +5,24 @@ namespace WPBin\Web\ServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class IoC extends ServiceProvider {
+    protected $bind_mappings = [
+        'WPBin\Core\Usecase\Tag\CreateRepository'
+            => 'WPBin\Web\Repository\Tag\Create',
+
+        'WPBin\Core\Tool\Validator\Tag'
+            => 'WPBin\Web\Tool\Validator\Tag',
+
+        'WPBin\Core\Tool\Validator\Paste'
+            => 'WPBin\Web\Tool\Validator\Paste',
+
+        'WPBin\Core\Tool\Validator\User'
+            => 'WPBin\Web\Tool\Validator\User',
+    ];
+
     public function register()
     {
-        // $this->app->bind(
-        //     'WPBin\Web\Repository\TagInterface',
-        //     'WPBin\Web\Repository\Tag'
-        // );
+        foreach ($this->bind_mappings as $from => $to) {
+            $this->app->bind($from, $to);
+        }
     }
 }
