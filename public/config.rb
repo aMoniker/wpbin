500729675750232138c4d4bfcb68cfb05deef248
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-09 00:38:14 -0400

    add meyer-reset

--- public/config.rb
+++ public/config.rb
@@ -2,6 +2,7 @@
 
 add_import_path 'bower_components/font-awesome/scss';
 add_import_path 'bower_components/foundation/scss';
+add_import_path 'bower_components/meyer-reset/stylesheets';
 
 css_dir = 'css'
 sass_dir = 'sass'
