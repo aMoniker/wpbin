1d93cbd3a928e7f7ed05ff32e726a97d9f395bc4
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-09 15:12:13 -0400

    working paste creation

---
+++ app/controllers/PasteController.php
@@ -0,0 +1,41 @@
+<?php
+
+use WPBin\Core\Entity\PasteRepository;
+
+class PasteController extends BaseController {
+
+    protected $repo;
+
+    public function __construct(PasteRepository $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function create()
+    {
+        // validate inputs
+        $rules = [
+            'paste' =>  ['required'],
+        ];
+
+        $error_messages = array(
+            'paste.required' => 'Fill in the Bin, yo!',
+        );
+
+        $validator = Validator::make(Input::all(), $rules, $error_messages);
+
+        if ($validator->fails()) {
+            return Redirect::route('home')->withErrors($validator);
+        }
+
+        // make the paste
+        $paste = $this->repo->create(
+            Input::get('paste-title'),
+            Input::get('paste')
+        );
+
+        // redirect to the hash url
+        return Redirect::route('hash', array('hash' => $paste->get('hash')));
+    }
+
+}
