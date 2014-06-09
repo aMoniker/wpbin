5e43152f0180e92381f7b03426d7cffa413fe301
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-08 23:45:37 -0400

    add compass config

---
+++ public/config.rb
@@ -0,0 +1,10 @@
+# compass config
+
+add_import_path 'bower_components/font-awesome/scss';
+add_import_path 'bower_components/foundation/scss';
+
+css_dir = 'css'
+sass_dir = 'sass'
+
+environment = :development
+output_style = :expanded
