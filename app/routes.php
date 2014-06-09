141fb16f5ecc88b48d3fc136ae90084af6f1b676
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-09 15:10:50 -0400

    name routes

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,11 @@
 <?php
 
-Route::get('/', 'HomeController@home');
+Route::get('/', ['as' => 'home', 'uses' => 'HomeController@home']);
 
-Route::get('/{hash}', function($hash) {
+Route::post('/', 'PasteController@create');
+
+Route::get('/{hash}', ['as' => 'hash', function($hash) {
     $hash = strtolower($hash);
-})
+    var_dump('hash route', $hash);
+}])
 ->where(['hash' => '[A-Za-z0-9]{4,}']);
