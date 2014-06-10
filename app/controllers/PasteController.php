f17d76662c16652b17a65005c31b96cbb24895e8
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-09 22:11:33 -0400

    parse content for related tags, insert as markdown links, pass used tags to view

--- app/controllers/PasteController.php
+++ app/controllers/PasteController.php
@@ -1,15 +1,17 @@
 <?php
 
 use WPBin\Core\Entity\PasteRepository;
+use WPBin\Core\Entity\TagRepository;
 
 class PasteController extends BaseController
 {
 
     protected $repo;
 
-    public function __construct(PasteRepository $repo)
+    public function __construct(PasteRepository $repo, TagRepository $tagrepo)
     {
         $this->repo = $repo;
+        $this->tagrepo = $tagrepo; // this is temporary, should be moved to the proper place
     }
 
     public function create()
@@ -49,7 +51,31 @@ class PasteController extends BaseController
             throw new NotFoundHttpException;
         }
 
-        return View::make('paste', ['paste' => $paste]);
+        // TODO: this should go somewhere else
+
+        $paste_tags = [];
+        $tag_entities = $this->tagrepo->getAll();
+        $paste_content = $paste->get('content');
+
+        foreach ($tag_entities as $tag) {
+            $name = $tag->get('name');
+            $regex = "/([^a-z0-9_])$name([^a-z0-9_])/i";
+            if (!preg_match($regex, $paste_content)) { continue; }
+
+            $url = $tag->get('url');
+            $paste_content = preg_replace(
+                $regex, "\$1[$name]({$url})\$2", $paste_content
+            );
+
+            $paste_tags[] = $tag;
+        }
+
+        $paste->set('content', $paste_content);
+
+        return View::make('paste', [
+            'paste' => $paste,
+            'tags'  => $paste_tags,
+        ]);
     }
 
 }
